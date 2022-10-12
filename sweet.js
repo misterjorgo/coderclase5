c19675f397c0103f6c0c6b96c045af49e9da4713
Jorge Flores <108765698+misterjorgo@users.noreply.github.com>
2022-10-12 00:25:49 -0500

    sweet.js
    sweet.js

---
+++ sweet.js
@@ -0,0 +1,5 @@
+const respuesta = prompt("Que te gustan los zorros o los caballos?");
+
+respuesta === "caballo"
+  ? alert("Genial, ami tambien me gustan los caballos")
+  : alert("Genial, tambien son un gran animal haha");
